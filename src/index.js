12717fa25bea4eea3c7f4769cf6508d5fff051d1
Arun Seehra <seehra.a@gmail.com>
2018-11-19 09:22:58 -0600

    Add basic express boilerplate

---
+++ src/index.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const cors = require('cors');
+const express = require('express');
+const morgan = require('morgan');
+
+const { PORT, CLIENT_ORIGIN } = require('../config');
+
+const app = express();
+
+app.use(
+  morgan(process.env.NODE_ENV === 'production' ? 'common' : 'dev', {
+    skip: () => process.env.NODE_ENV === 'test',
+  }),
+);
+
+app.use(cors({ origin: CLIENT_ORIGIN }));
+
+/* eslint-disable no-console */
+function main(port = PORT) {
+  const server = app
+    .listen(port, () => {
+      console.info(`App listening on port ${server.address().port}`);
+    })
+    .on('error', (err) => {
+      console.error('Express failed to start');
+      console.error(err);
+    });
+}
+/* eslint-enable no-console */
+
+if (require.main === module) {
+  main();
+}
+
+module.exports = { app };
