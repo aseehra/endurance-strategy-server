df72ee1a0e481337f21ebf3f51f08db6f693968f
Arun Seehra <seehra.a@gmail.com>
2018-11-19 19:05:01 -0600

    Add GET /races/:id/entries endpoint
    This will fetch all the entries that took part in a race, and get their "most
    recent" (final) positions.

--- src/routers/racesRouter.js
+++ src/routers/racesRouter.js
@@ -12,9 +12,27 @@ router.get('/', (req, res, next) => {
     .then((allRaces) => {
       const serialized = allRaces.map(race => ({
         ...race,
-        links: { self: `${req.baseUrl}/${race.id}` },
+        links: {
+          self: `${req.baseUrl}/${race.id}`,
+          entries: `${req.baseUrl}/${race.id}/entries`,
+        },
       }));
       res.json({ races: serialized });
+    })
+    .catch(next);
+});
+
+router.get('/:id/entries', (req, res, next) => {
+  const { id } = req.params;
+  races
+    .fetchEntries(id)
+    .then((entries) => {
+      const serialized = entries.map(entry => ({
+        ...entry,
+        links: { self: `${req.baseUrl}/${id}/entries/${entry.id}` },
+      }));
+
+      res.json({ entries: serialized });
     })
     .catch(next);
 });
